7021b02285de307eba2b601853cf5e1376b45a64
ineedajobpls <winstongate1051@gmail.com>
2026-04-24 15:17:56 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = int(input('enter a number'))
+
+if number > 0 :
+    print("your number is positive")
+if number < 0 :
+    print("your number is negative")
+if number == 0 :
+    print("your number is 0")
