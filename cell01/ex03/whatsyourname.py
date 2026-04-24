0dcceb08dd27564bc707928717425e8e23a3fb42
ineedajobpls <winstongate1051@gmail.com>
2026-04-24 14:31:21 +0700

    Update whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+first_name = (input( "whats your first name?: " ))
+last_name = (input('whast your last name?: '))
+
+
+your_name = (first_name + ' ' + last_name)
+
+print ('nice to meet you' +' ' + your_name)
