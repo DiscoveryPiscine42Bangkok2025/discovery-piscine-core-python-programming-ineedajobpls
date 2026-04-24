b64f37af66843bb697b13b08e1917d09722b1784
ineedajobpls <winstongate1051@gmail.com>
2026-04-24 15:44:08 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter your first number"))
+num2 = int(input("enter your second number"))
+
+a = (num1 * num2)
+
+print(num1 ,"x", (num2), ' =', a)
+
+if a > 0 :
+    print("your number is positive")
+if a < 0 :
+    print("your number is negative")
+if a == 0 :
+    print("your number is both positive and negative")
+
