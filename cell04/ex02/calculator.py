3cb35f2a64695825fc3d4ab2ea79bda4553c3a01
ineedajobpls <winstongate1051@gmail.com>
2026-04-25 11:37:34 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+n = int(input('enter your first number: '))
+i = int(input('enter your second number: '))
+print('thankyou!')
+
+print (n , ' + ' , i ,  ' = ' , (n+i))
+print (n , ' - ' , i ,  ' = ' , (n-i))
+print (n , ' x ' , i ,  ' = ' , (n*i))
+print (n , ' / ' , i ,  ' = ' , (n/i))
+
+
