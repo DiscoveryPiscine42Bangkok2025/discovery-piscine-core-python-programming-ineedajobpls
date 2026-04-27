d6a00af8e2d60f829e6e57748e4b9df870cd33ae
ineedajobpls <winstongate1051@gmail.com>
2026-04-28 00:30:02 +0700

    Add files via upload

---
+++ cell00/ex00/main.py
@@ -0,0 +1,13 @@
+from checkmate.py import checkmate
+
+def main():
+    board = [
+        "R...",
+        ".K..",
+        "....",
+        "...."
+    ]
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
